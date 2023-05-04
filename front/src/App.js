746d5cf0b111fcaabc02b3edc8ee4cf9223d757b
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-05-04 06:29:42 +0300

    mise à jour du json

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Sentenses from "./pages/Sentenses";
 import Units from "./pages/Units";
+import vocabulary from "./pages/Vocabulary";
 import Error from "./pages/Error";
 import Navigation from "./components/Navigation";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/sentences" element={<Sentenses />}></Route>
           <Route path="/units" element={<Units />}></Route>
+          <Route path="/vocabulary" element={<vocabulary />}></Route>
           <Route path="*" element={<Error />}></Route>
         </Routes>
       </BrowserRouter>
